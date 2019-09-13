d594b009cff01ab60778586889f083a4bf8e2176
SuchiSmita-N <suchismita.nayak@springer.com>
2019-09-13 14:05:17 +0200

    Slice added instead of array

--- firstgo/sum_test.go
+++ firstgo/sum_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 func Testsum(t *testing.T) {
-	nos := [5]int{1,2,3,4,5}
+	t.Run("Slice: Arry of any size", func (t *testing.T) {
+	nos := []int{1,2,3,4,5}
 
 	get := sum(nos)
 	result:= 15
@@ -11,10 +12,11 @@ func Testsum(t *testing.T) {
     if result != get {
 		t.Errorf ("get %d expected %d, Given %v ", get, result, nos)
     }
+}) 
 	
 }
 
-func sum(nos [5]int) int {
+func sum(nos []int) int {
 	sum := 0
 	for _,nos := range nos{
 		sum += nos
