fe2a1184b9a9b92b206608e0df7162538450a13b
SuchiSmita-N <suchismita.nayak@springer.com>
2019-12-11 10:30:36 +0100

    Added one more example

--- firstgo/struct_test.go
+++ firstgo/struct_test.go
@@ -11,6 +11,20 @@ func Testperimeter(t *testing.T) {
 	}
 }
 
+func TestArea(t *testing.T) {
+
+	result := area(5.0, 6.0)
+	expect := 30.0
+
+	if result != expect{
+		t.errorf("result %.2f expect %.2f" result, expect)
+	}	
+}
+
 func perimeter(width float64, height float64) float64 {
     return 2 * (width + height)
+}
+
+func area(width float64, height float64) float64 {
+    return  (width * height)
 }
