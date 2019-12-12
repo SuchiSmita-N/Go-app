8aa5fe609ff48e28037b5409f4861cfcb86df857
SuchiSmita-N <suchismita.nayak@springer.com>
2019-12-12 10:27:26 +0100

    modifies errorf

--- firstgo/struct_test.go
+++ firstgo/struct_test.go
@@ -7,30 +7,20 @@ type rectangle struct {
 	height float64
 }
 
+func (r Rectangle) Area() float64  {
+    return 0
+}
+
 func Testperimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	result := perimeter(rectangle)
 	expect := 40.0	
 
 	if result != expect {
-		t.errorf("result %.2f expect %.2f", result, expect)
+		t.Errorf("result %.2f expect %.2f", result, expect)
 	}
-}
-
-func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	result := area(rectangle)
-	expect := 30.0
-
-	if result != expect{
-		t.errorf("result %.2f expect %.2f" result, expect)
-	}	
 }
 
 func perimeter(rectangle Rectangle) float64 {
     return 2 * (rectangle.width + rectangle.height)
-}
-
-func area(rectangle Rectangle) float64 {
-    return  (rectangle.width * rectangle.height)
 }
